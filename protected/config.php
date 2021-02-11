fd692e351959f44e439c071ff5f65eff7c90701f
Benny <74463543+ilyBenny@users.noreply.github.com>
2021-02-11 13:16:13 +0000

    Update config.php

--- protected/config.php
+++ protected/config.php
@@ -1,1 +1,6 @@
+  define('DIRECTORY', ""); # Directory of where this file and uploads folder is located.
+  define('PASSWORD', ""); # Your password
+  define('DOMAIN', ""); # Your domain NOT THE URL
 
+  define('EMBED', true);
+  define('EMBED_TITLE', ""); # The title of the embedded message or twitter card.
