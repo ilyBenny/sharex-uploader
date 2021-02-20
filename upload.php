a891b58c69c42b522a74da628ab0203496d4561a
Benny <74463543+ilyBenny@users.noreply.github.com>
2021-02-20 17:32:05 +0000

    Update upload.php

--- upload.php
+++ upload.php
@@ -3,7 +3,7 @@
   require_once __DIR__ . '/protected/config.php';
 
   $protocol = isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] != "off" ? "https" : "http" . "://";
-  $hash = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 5);
+  $hash = substr(str_shuffle(str_repeat("0123456789abcdefghijklmnopqrstuvwxyz", 5)), 0, 8);
 
   if ( ! isset($_POST['password']) || $_POST['password'] !== PASSWORD) {
       die('Incorrect password.');
@@ -15,9 +15,9 @@
     $filelocation = __DIR__ . "/uploads/$hash.$type";
     if(move_uploaded_file($_FILES['file']['tmp_name'], "uploads/$hash.$type")) {
       if (EMBED == true) {
-        echo $protocol . DOMAIN . "/view?f=$hash.$type";
+        echo $protocol . DOMAIN . DIRECTORY . "/?f=$hash.$type";
       } else {
-        echo $protocol . DOMAIN . "/uploads/$hash.$type";
+        echo $protocol . DOMAIN . DIRECTORY . "/uploads/$hash.$type";
       }
     } else {
         echo "Failed to upload your file.";
