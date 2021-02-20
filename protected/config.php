b30067542bb651e32b6ae990c4c342daa685aea8
Benny <74463543+ilyBenny@users.noreply.github.com>
2021-02-20 17:34:13 +0000

    Update config.php

--- protected/config.php
+++ protected/config.php
@@ -1,8 +1,9 @@
 <?php
 
-define('DIRECTORY', ""); # Directory of where this file and uploads folder is located.
+define('DIRECTORY', ""); # Directory of where the upload.php file and uploads folder is located.
 define('PASSWORD', ""); # Your password
 define('DOMAIN', ""); # Your domain NOT THE URL
 
-define('EMBED', true);
+define('EMBED', true); # If you want to show the image in an embed.
 define('EMBED_TITLE', ""); # The title of the embedded message or twitter card.
+define('EMBED_COLOUR', ""); # The colour of the embedded message.
